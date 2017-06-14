32e06122143f7af35fd5b13010d12144e9b61f2d
xie <xiehp@users.noreply.github.com>
2017-06-14 23:50:51 +0800

    修改jvm参数

--- AnimeShotSite-timer/bat/startTimer.bat
+++ AnimeShotSite-timer/bat/startTimer.bat
@@ -4,12 +4,12 @@ echo %~dp0
 cd %~dp0
 
 # set the java home path
-set "app_java_home_path=D:\work\java\X64\jdk1.8.0_66"
+set "app_java_home_path=D:\work\java\X64\jdk1.8.0_131"
 
 
 set "app_java_exe=%app_java_home_path%\bin\java"
 set "app_java_ext_lib_path=%app_java_home_path%\jre\lib\ext"
-set "JAVA_OPTION= -Xmx400M -Xms200M -XX:MetaspaceSize=100M -XX:MaxMetaspaceSize=150M "
+set "JAVA_OPTION= -Xms200M -Xmx250M -XX:MetaspaceSize=100M -XX:MaxMetaspaceSize=150M "
 
 %app_java_exe% -version
 %app_java_exe% -Djava.ext.dirs=%app_java_ext_lib_path%;AnimeLib;OtherLib; %JAVA_OPTION% -Dspring.profiles.active=productRemote -Dlogback.configurationFile=conf/logback.xml -classpath conf;AnimeShotSite-timer-0.0.1-SNAPSHOT.jar;AnimeLib; xie.animeshotsite.timer.main.MainTimer
