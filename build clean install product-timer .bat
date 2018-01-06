90997c14ffbc76a03f77e6eea8c660b07586d1ea
xie <xiehp@users.noreply.github.com>
2018-01-06 21:56:15 +0800

    更新

--- build clean install product-timer .bat
+++ build clean install product-timer .bat
@@ -4,7 +4,7 @@ echo 先移动当前cd位置到文件所在目录
 cd %~dp0
 
 echo  开始build
-mvn -Pproduct,timer -B -Dmaven.test.skip=true -DskipTests --threads 2 -s D:\work\maven\maven-setting.xml clean install
+mvn -Pproduct,timer -B -U -e  -Dmaven.test.skip=true -DskipTests --threads 2 -s D:\work\maven\maven-setting.xml clean install
 
 echo 回到原来cd所在目录
 cd %nowPath%
